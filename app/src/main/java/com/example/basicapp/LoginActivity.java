0eed977511213f4afd923b20663eebde811ed2cb
crpoudyal <crpoudyal@gmail.com>
2022-02-24 19:59:06 +0545

    initial commit

---
+++ app/src/main/java/com/example/basicapp/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.basicapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.relative_layout);
+
+    }
+}
